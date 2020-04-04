5c7a82958411bae75aac488ddc7a6763ba020fc4
shpitalniy <shpitalniyvlad@gmail.com>
2020-04-05 02:21:25 +0300

    Add styles

--- src/modules/Layout/styledComponent.js
+++ src/modules/Layout/styledComponent.js
@@ -1,26 +1,33 @@
 import styled from 'styled-components';
+import background from '../../../asserts/background.jpg';
 
 export const App = styled.div`
     width: 100%;
     height: 100%;
+    background-image: url(${background});
     display: flex;
     align-items: center;
     justify-content: center;
 `;
 
 App.Component = styled.div`
-    width: 50%;
-    height: 50%;
+    min-width: 50%;
+    min-height: 50%;
     display: flex;
     align-items: center;
     justify-content: center;
 `;
 
 export const Button = styled.button`
-    min-width: 100px;
-    min-height: 50px;
+    min-width: 300px;
+    min-height: 100px;
     padding: 5px;
+    color: white;
+    text-transform: uppercase;
+    font-weight: bold;
+    font-size: 20px;
+    font-family: Verdana;
     border: none;
     border-radius: 10px;
-    background: #31c352;
+    background: #20bf6b;
 `;
--- webpack.config.js
+++ webpack.config.js
@@ -4,45 +4,56 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-    entry: "./src/index.js",
-    output: {
-        filename: "index.[hash:8].js",
-        path: path.resolve(__dirname, "dist"),
-    },
-    plugins: [
-        new CleanWebpackPlugin(),
-        new HtmlWebpackPlugin({
-            template: './public/index.html',
-            filename: 'index.html',
-        }),
-        new MiniCssExtractPlugin({
-          filename: 'index.[hash:8].css',
-        })
-      ],
-      module: {
-        rules: [
-          {
-            test: /\.(js|jsx)$/,
-            exclude: /node_modules/,
-            use: {
-              loader: "babel-loader"
-            },
-          },
-          {
-            test: /\.(less|css)$/,
-            use: [
-              MiniCssExtractPlugin.loader,
-              'css-loader',
-              'less-loader'
-            ],
-          },
+  entry: "./src/index.js",
+  output: {
+    filename: "index.[hash:8].js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+      filename: 'index.html',
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'index.[hash:8].css',
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        },
+      },
+      {
+        test: /\.(less|css)$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'less-loader'
         ],
       },
-      devtool: 'source-map',
-      devServer: {
-        port: 3000,
-        index: "./dist/index.html",
-        hotOnly: true,
-        historyApiFallback: true,
-    },
+      {
+        test: /\.(png|svg|jpg|gif)$/i,
+        loader: 'file-loader',
+        options: {
+          name: '[path][name].[ext]',
+        },
+      },
+      {
+        test: /\.json$/,
+        loader: "json-loader",
+      },
+    ],
+  },
+  devtool: 'source-map',
+  devServer: {
+    port: 3000,
+    index: "./dist/index.html",
+    hotOnly: true,
+    historyApiFallback: true,
+  },
 }
